ac436dda20f8df7cdae46c82a345f786e9f57328
Gilvan André Gobbato <gilvangobbato@gmail.com>
2019-03-22 12:51:14 -0300

    Conversor de moedas

---
+++ conversor_de_moedas/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const request = "https://api.hgbrasil.com/finance/quotations?format=json&key=71d8957d";
+
+void main() {
+  runApp(MaterialApp(
+    home: Container(
+
+    ),
+  ));
+}
