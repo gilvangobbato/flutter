3ebae7862367f6575967a9bc86679f69bfe13294
Gilvan André Gobbato <gilvangobbato@gmail.com>
2019-04-15 22:36:42 -0300

    Salvando e editando contato

--- agenda_contatos/lib/ui/contact_page.dart
+++ agenda_contatos/lib/ui/contact_page.dart
@@ -1,9 +1,10 @@
+import 'dart:io';
+
 import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
 class ContactPage extends StatefulWidget {
   final Contact contact;
-
   ContactPage({this.contact});
 
   @override
@@ -11,6 +12,11 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _phoneController = TextEditingController();
+  final _nameFocus = FocusNode();
+  bool _userEdited = false;
   Contact _editedContact;
 
   @override
@@ -20,6 +26,9 @@ class _ContactPageState extends State<ContactPage> {
       _editedContact = new Contact();
     } else {
       _editedContact = Contact.fromMap(widget.contact.toMap());
+      _nameController.text = _editedContact.name;
+      _emailController.text = _editedContact.email;
+      _phoneController.text = _editedContact.phone;
     }
   }
 
@@ -32,11 +41,70 @@ class _ContactPageState extends State<ContactPage> {
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
-          onPressed: (){
-
-          },
+        onPressed: () {
+          if (_editedContact.name != null && _editedContact.name.isNotEmpty) {
+            Navigator.pop(context, _editedContact);
+          }else{
+            FocusScope.of(context).requestFocus(_nameFocus);
+          }
+        },
         child: Icon(Icons.save),
         backgroundColor: Colors.red,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(5.0),
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              child: Container(
+                width: 140.0,
+                height: 140.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: _editedContact.img != null
+                          ? FileImage(File(_editedContact.img))
+                          : AssetImage("images/padrao.jpg")),
+                ),
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Nome",
+              ),
+              onChanged: (text) {
+                _userEdited = true;
+                setState(() {
+                  _editedContact.name = text;
+                });
+              },
+              controller: _nameController,
+              focusNode: _nameFocus,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Email",
+              ),
+              onChanged: (text) {
+                _userEdited = true;
+                _editedContact.email = text;
+              },
+              keyboardType: TextInputType.emailAddress,
+              controller: _emailController,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Phone",
+              ),
+              onChanged: (text) {
+                _userEdited = true;
+                _editedContact.phone = text;
+              },
+              keyboardType: TextInputType.phone,
+              controller: _phoneController,
+            )
+          ],
+        ),
       ),
     );
   }
